b467cf5a1eb746265ae8a3384d47efbd87e06ac5
ArturoGJ <arturogarridoj@gmail.com>
2023-02-07 21:09:33 -0600

    Trying with different url for pdfs

--- js/index.js
+++ js/index.js
@@ -21,7 +21,7 @@ Array.from(aEditions).forEach((edition) => {
 function renderPDF(id) {
   // If absolute URL from the remote server is provided, configure the CORS
   // header on that server.
-  var url = `./pdfs/${id}.pdf`;
+  var url = `/UitwaaieMag/pdfs/${id}.pdf`;
 
   // Loaded via <script> tag, create shortcut to access PDF.js exports.
   var pdfjsLib = window['pdfjs-dist/build/pdf'];
