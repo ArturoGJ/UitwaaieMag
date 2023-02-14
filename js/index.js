918daa46741130439e1e99286579a47afecf63a8
ArturoGJ <arturogarridoj@gmail.com>
2023-02-13 19:23:27 -0600

    Added the ability to change the language of the web page

--- js/index.js
+++ js/index.js
@@ -164,7 +164,7 @@ function renderPDF(id) {
 // Start of links handler
 const aboutLink = document.getElementById('about-link');
 const homeLink = document.getElementById('home-link');
-const logo = document.getElementById('logo');
+const logo = document.getElementById('logo-img');
 
 const aboutContent = document.getElementById('about-content');
 const homeContent = document.getElementById('home-content');
@@ -208,3 +208,109 @@ function openMenu() {
   let menu = document.getElementById('menus');
   menu.classList.toggle('menu-open');
 }
+
+// Switch language
+function onChangeLanguage(value) {
+  let menu = getMenuElementsAndText();
+  let about = getAboutElementsAndText();
+
+  let language = value === 'NL' || value === 'EN' ? value : 'NL';
+
+  for (const key in menu) {
+    let element = menu[key].element;
+    let text = menu[key][`text${language}`];
+    element.innerText = text;
+  }
+  for (const key in about) {
+    let element = about[key].element;
+    let text = about[key][`text${language}`];
+    element.innerText = text;
+  }
+}
+
+function getMenuElementsAndText() {
+  return {
+    homeLink: {
+      element: document.getElementById('home-link'),
+      textNL: 'Startpagina',
+      textEN: 'Home',
+    },
+    aboutLink: {
+      element: document.getElementById('about-link'),
+      textNL: 'Over Ons',
+      textEN: 'About',
+    },
+    submitLink: {
+      element: document.getElementById('submit-link'),
+      textNL: 'Inzenden',
+      textEN: 'Submit',
+    },
+    contactLink: {
+      element: document.getElementById('contact-link'),
+      textNL: 'Contact',
+      textEN: 'Contact',
+    },
+  };
+}
+
+function getAboutElementsAndText() {
+  return {
+    aboutTitle: {
+      element: document.getElementById('about-title'),
+      textNL: 'Over Ons',
+      textEN: 'About Us',
+    },
+    firstParagraph: {
+      element: document.getElementById('about-first-paragraph'),
+      textNL:
+        'Uitwaaien Magazine is een platform voor kunst, cultuur en sociale verbinding. Uitwaaien Magazine, gevestigd in Wageningen, toont amateur- en professionele kunstenaars uit de regio en omgeving die bruggen willen slaan en een frisse wind laten waaien door de geest van lezers en donateurs.',
+      textEN:
+        'Uitwaaein Magazine is a platform for art, culture, and social connection. Located in Wageningen, Uitwaaien Magazine features amateur and professional artists from the area and surroundings seeking to create bridges and bring a breath of fresh wind to the mind of readers and contributors.',
+    },
+    secondParagraph: {
+      element: document.getElementById('about-second-paragraph'),
+      textNL:
+        'Uitwaaien Magazine is een non-profit organisatie die gelooft in kunst als middel tot verbinding. Het tijdschrift wordt twee keer per jaar gedrukt, in de zomer en in de winter, waarbij elke editie streeft naar een diverse en inclusieve inhoud. Iedereen en alle soorten artistieke disciplines zijn welkom.',
+      textEN:
+        'Uitwaaien Magazine is a non-profit organisation that believes in art as a means of connection. The magazine is printed twice a year, summer and winter, with each edition aiming for diverse and inclusive content. Everyone and all kinds of artistic disciplines are welcome.',
+    },
+    boardTitle: {
+      element: document.getElementById('about-board-title'),
+      textNL: 'Bestuur',
+      textEN: 'Board',
+    },
+    boardPm: {
+      element: document.getElementById('about-board-pm'),
+      textNL: 'Projectleider: Marta Vallvé Odena (voorzitter)',
+      textEN: 'Project manager: Marta Vallvé Odena (Chair)',
+    },
+    boardCuration: {
+      element: document.getElementById('about-board-curation'),
+      textNL: 'Curatie: Fernando Gabriel',
+      textEN: 'Curation: Fernando Gabriel',
+    },
+    boardDesign: {
+      element: document.getElementById('about-board-design'),
+      textNL: 'Ontwerp: Mattia Bosoni',
+      textEN: 'Design: Mattia Bosoni',
+    },
+    boardPr: {
+      element: document.getElementById('about-board-pr'),
+      textNL: 'PR - sociale media: Imme Koster',
+      textEN: 'PR - social media: Imme Koster',
+    },
+    boardTreasurer: {
+      element: document.getElementById('about-board-treasurer'),
+      textNL: 'Penningmeester: Edmund Bell',
+      textEN: 'Treasurer: Edmund Bell',
+    },
+    aboutContact: {
+      element: document.getElementById('about-contact'),
+      textNL: 'Heb je interesse om je als vrijwilliger of bestuurslid bij het project aan te sluiten? Stuur een e-mail naar uitwaaien.magazine@gmail.com',
+      textEN: 'Are you interested in joining the project as a volunteer or board member? Send an email to uitwaaien.magazine@gmail.com',
+    },
+  };
+}
+
+// Initialize page with default language
+onChangeLanguage('NL'); // Initializing with default language NL
